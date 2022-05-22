1838629da0623142601934efff420d6a5b45ad49
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-05-22 18:03:11 -0300

    extraido funcao testaCopiaEReferencia

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,65 @@
 fun main() {
     println("Bem vindo ao Nice Bank!!!")
 
-    Conta() // Instância de uma classe
+    val contaPablo = Conta()  // Apontando para um objeto, o que me permite acessar e alterar os atributos dentro dele.
+    contaPablo.titular = "Pablao Mitico"
+    contaPablo.numero = 1000
+    contaPablo.saldo = 10000.0
 
-//    val contaPablo = Conta()  // Apontando para um objeto, o que me permite acessar e alterar os atributos dentro dele.
-//    contaPablo.titular = "Pablao Mitico"
-//    contaPablo.numero = 1000
-//    contaPablo.saldo = 10000.0
-//    println(contaPablo.titular)
-//    println(contaPablo.numero)
-//    println(contaPablo.saldo)
-//
-//    val contaMaria = Conta()
-//    contaMaria.titular = "Maria Rafaela"
-//    contaMaria.numero = 1001
-//    contaMaria.saldo = 12000.0
+
+    val contaMaria = Conta()
+    contaMaria.titular = "Maria Rafaela"
+    contaMaria.numero = 1001
+    contaMaria.saldo = 12000.0
+
 //    println(contaMaria.titular)
 //    println(contaMaria.numero)
 //    println(contaMaria.saldo)
+//
+//    println(contaPablo.titular)
+//    println(contaPablo.numero)
+//    println(contaPablo.saldo)
 
-    val numeroX = 10 // criei uma variável e passei o numero 10 por parâmetro
-    var numeroY = numeroX // criei uma segunda variável que recebe por parâmetro a variavel numeroX (anterior)
-    numeroY++ // incerementei a variavel numeroY sem alterara o valor de numeroX
-
-    /* porque isso acontece?
-    quando instancio uma referencia de variavel primitiva,
-    estou enviando apenas uma cópia da mesma e não uma referência da mesma.
-     */
-
-    println(numeroX)
-    println(numeroY)
-
+    testaCopiaEReferencias()
 }
 
 class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
+}
+
+fun testaCopiaEReferencias(){
+
+    Conta() // Instância de uma classe Conta()
+
+    val numeroX = 10
+    var numeroY = numeroX // criei uma segunda variável que recebe por parâmetro a variavel numeroX (anterior)
+    numeroY++ // incrementei a variavel numeroY sem alterar o valor de numeroX
+
+    println(numeroX)
+    println(numeroY)
+
+    /* o numeroX não é alterado porque isso acontece?
+    quando instancio uma referencia de variavel primitiva,(Float, Int, Double ETC)
+    estou enviando apenas uma cópia da mesma e não uma referência da mesma.
+     */
+
+
+    val contaLucas = Conta()
+    contaLucas.titular = "Lucas"
+    var contaCarol = contaLucas
+    contaCarol.titular = "Carol"
+
+    println("titular conta Lucas: ${contaLucas.titular}")
+    println("titular conta Lucas: ${contaCarol.titular}")
+
+    println(contaLucas) // a hash gerada é alterada quando mudamos o objeto
+    println(contaCarol)
+
+    /* A contaLucas é alterada, porque isso acontece?
+    quando instancio uma referência de objeto, ao assinar um novo valor o mesmo também é alterado
+     */
 }
 
 fun testaLacos() {
