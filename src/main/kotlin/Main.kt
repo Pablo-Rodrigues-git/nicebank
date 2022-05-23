a352798287327f81e0f4a984f71c94088321aa76
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-05-23 20:45:36 -0300

    transformado a variavel saldo em privada, criado getter e setter para criar uma copia da variavel e adicionar lógica

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,53 +4,55 @@ fun main() {
     val contaPablo = Conta()  // Apontando para um objeto, o que me permite acessar e alterar os atributos dentro dele.
     contaPablo.titular = "Pablao Mitico"
     contaPablo.numero = 1000
-    contaPablo.saldo = 100.0
+    contaPablo.setSaldo(100.0)
 
 
     val contaMaria = Conta()
     contaMaria.titular = "Maria Rafaela"
     contaMaria.numero = 1001
-    contaMaria.saldo = 100.0
+    contaMaria.setSaldo(100.0)
 
     println(contaMaria.titular)
     println(contaMaria.numero)
+    println(contaMaria.getSaldo())
 
     println(contaPablo.titular)
     println(contaPablo.numero)
+    println(contaPablo.getSaldo())
 
 
-    // deposito
-    println("Depositando na conta do Pablo")
-    contaPablo.deposita(100.0)
-    println(contaPablo.saldo)
-
-    println("Depositando na conta da Maria")
-    contaMaria.deposita(100.0)
-    println(contaMaria.saldo)
-
-    // saque
-    println("Sacando na conta do Pablo")
-    contaPablo.saca(50.0)
-    println("Saldo atual: ${contaPablo.saldo}")
-
-    println("Sacando na conta da Maria")
-    contaMaria.saca(50.0)
-    println("Saldo atual: ${contaMaria.saldo}")
-
-    println("transferência da conta do Pablo para Maria")
-    if (contaPablo.transfere(100.0, contaMaria)){
-        println("Transferencia realizada com sucesso")
-    } else {
-        println("Falha na transferência")
-    }
-    println(contaPablo.saldo)
-    println(contaMaria.saldo)
+//    // deposito
+//    println("Depositando na conta do Pablo")
+//    contaPablo.deposita(100.0)
+//    println(contaPablo.saldo)
+//
+//    println("Depositando na conta da Maria")
+//    contaMaria.deposita(100.0)
+//    println(contaMaria.saldo)
+//
+//    // saque
+//    println("Sacando na conta do Pablo")
+//    contaPablo.saca(50.0)
+//    println("Saldo atual: ${contaPablo.saldo}")
+//
+//    println("Sacando na conta da Maria")
+//    contaMaria.saca(50.0)
+//    println("Saldo atual: ${contaMaria.saldo}")
+//
+//    println("transferência da conta do Pablo para Maria")
+//    if (contaPablo.transfere(100.0, contaMaria)){
+//        println("Transferencia realizada com sucesso")
+//    } else {
+//        println("Falha na transferência")
+//    }
+//    println(contaPablo.saldo)
+//    println(contaMaria.saldo)
 }
 
 class Conta {
     var titular = ""
     var numero = 0
-    var saldo = 0.0
+    private var saldo = 0.0
 
     fun deposita (valor: Double){ // tornamos essa função membro do objeto conta, não sendo mais necessário instanciar a conta por parâmetro
 
@@ -68,10 +70,20 @@ class Conta {
     fun transfere (valor: Double, destino: Conta):  Boolean {
         if (saldo >= valor){
             saldo -= valor
-            destino.saldo + valor
+            destino.deposita(valor)
             return true
         }
             return false
+    }
+
+    fun getSaldo(): Double{
+        return saldo
+    }
+
+    fun setSaldo(valor: Double){
+        if(saldo > 0){
+            saldo = valor
+        }
     }
 }
 
@@ -118,13 +130,11 @@ fun testaLacos() {
         val numeroConta = 1000 + i
         var saldo = i + 10.0
 
-
         println("Titular $titular")
         println("Numero da Conta $numeroConta")
         println("Saldo R$ $saldo")
         println()
         i++
-
     }
 }
 
