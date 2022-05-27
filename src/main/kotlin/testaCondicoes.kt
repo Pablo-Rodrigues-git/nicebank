fcf6b283e2eeddc25c089cbc2a0e6dc29d7d15fa
Pablo-Rodrigues-git <pablo.rodrigues2015@outlook.com>
2022-05-26 23:36:55 -0300

    organizando codigo

---
+++ src/main/kotlin/testaCondicoes.kt
@@ -0,0 +1,14 @@
+fun testaCondicoes(saldo: Double) {
+    if (saldo >= 0) {
+        if (saldo == 0.0) {
+            println("Conta é neutra")
+
+        } else {
+            println("Conta é Positiva")
+        }
+    } else {
+        println("Conta é negativa ")
+    }
+
+
+}
