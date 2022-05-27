82c8d8d536ba619a10dbb94544db1b40eb7c5e38
Pablo-Rodrigues-git <pablo.rodrigues2015@outlook.com>
2022-05-26 23:49:15 -0300

    criado classe funcionando e o metodo bonificacao

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,15 @@
 fun main() {
     println("Bem vindo ao Nice Bank!!!")
+
+    val alex = Funcionario(
+        nome = "Alex", // properties
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
+
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salario ${alex.salario}")
+    println("bonificação ${alex.bonificacao()}")
 }
 
